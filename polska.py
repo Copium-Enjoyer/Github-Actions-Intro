42a21606a5725d47fa076569b93fc8f7001b473a
Copium-Enjoyer <olgierd.zagozda@gmail.com>
2023-09-19 16:00:35 +0200

    added get users functionality

---
+++ polska.py
@@ -0,0 +1,33 @@
+import requests
+import re
+import pandas as pd
+import datetime
+
+x = requests.get('https://lichess.org/')
+date = datetime.datetime.now()
+
+if x.status_code == 200:
+
+    m = re.search(r'("members":\d+\,"rounds":\d+)', x.text)
+    print(m.group(0))
+    n = re.findall(r'\d+', m.group(0))
+    print(n)
+
+
+    lichess = pd.read_csv("data\lichess.csv")
+
+    print(lichess)
+
+    data = {
+        'Date': [date],
+        'Players': [n[0]],
+        'Games': [n[1]]
+    }
+
+    pd.DataFrame(data).to_csv("data\lichess.csv", mode='a', index=False, header=False)
+
+    
+
+
+    # print(x.headers['data-count'])
+
